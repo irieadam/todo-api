d47bf219416c0d3133203e84d997df856538635c
irieadam <irieadam5@hotmail.com>
2016-04-29 14:58:07 +0200

    added post todos

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-    id: 1,
-    description: 'Meet mom for lunch',
-    completed: false
-}, {
-        id: 2,
-        description: 'go to market',
-        completed: false
-    }, {
-        id: 3,
-        description: 'ride bike',
-        completed: true
-    }];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send('Todo API root');
@@ -40,6 +32,14 @@ app.get('/todos/:id', function (req, res) {
             }
      );
 
+});
+
+// post new todo
+app.post('/todos', function(req, res){
+    var body = req.body;
+    body.id=todoNextId++;
+    todos.push(body);
+    res.json(body); 
 });
 
 app.listen(PORT, function () {
