eb5d8fbca4e158f406b20d578a3b2705615552d0
irieadam <irieadam5@hotmail.com>
2016-05-16 15:48:42 +0200

    add auth middle and findby toke class method

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
 var bcrypt = require('bcrypt');
+var middleware = require('./middleware.js')(db);
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -20,7 +21,7 @@ app.get('/', function (req, res) {
 // get all 
 // ? completed = true
 // ? q = searchstring
-app.get('/todos', function (req, res) {
+app.get('/todos', middleware.requireAuthentication ,function (req, res) {
     var queryParams = req.query;
     var where = {};
     
@@ -44,7 +45,7 @@ app.get('/todos', function (req, res) {
 });
 
 // get by id
-app.get('/todos/:id', function (req, res) {
+app.get('/todos/:id', middleware.requireAuthentication , function (req, res) {
     var todoId = parseInt(req.params.id,10);
     console.log('got id :' + todoId);
 
@@ -62,7 +63,7 @@ app.get('/todos/:id', function (req, res) {
 });
 
 // post new todo
-app.post('/todos', function(req, res){
+app.post('/todos' , middleware.requireAuthentication , function(req, res){
     var body = _.pick(req.body,'description', 'completed') ;
     
     if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
@@ -86,7 +87,7 @@ app.post('/todos', function(req, res){
 });
 
 
-app.delete('/todos/:id', function (req, res) {
+app.delete('/todos/:id', middleware.requireAuthentication , function (req, res) {
      var todoId  = parseInt(req.params.id,10);
         db.todo.destroy({
             where : {
@@ -106,7 +107,7 @@ app.delete('/todos/:id', function (req, res) {
      );  
 });
 
-app.put('/todos/:id', function (req, res) {
+app.put('/todos/:id', middleware.requireAuthentication , function (req, res) {
     var todoId  = parseInt(req.params.id,10);
     var body = _.pick(req.body,'description', 'completed') ;
     console.log(" Update object : " + JSON.stringify(body) + ' id ' + todoId);
